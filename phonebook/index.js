bd9c3633d2c4e03d6bc9f512caefd38d26712bb3
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2025-05-13 11:56:36 +0200

    add functionality for adding phonebook entries

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+app.use(express.json());
 let persons = [
 	{
 		id: "1",
@@ -59,6 +59,34 @@ app.delete("/api/persons/:id", (request, response) => {
 	persons = persons.filter((person) => person.id !== id);
 
 	response.status(204).end();
+});
+
+/*3-5*/
+
+const generateId = () => {
+	const maxId =
+		persons.length > 0 ? Math.max(...persons.map((p) => Number(p.id))) : 0;
+	return String(maxId + 1);
+};
+
+app.post("/api/persons", (request, response) => {
+	const body = request.body;
+
+	if (!body.name) {
+		return response.status(400).json({
+			error: "content missing",
+		});
+	}
+
+	const person = {
+		id: generateId(),
+		name: body.name,
+		number: body.number,
+	};
+
+	persons = persons.concat(person);
+
+	response.json(person);
 });
 
 /*3-1*/
